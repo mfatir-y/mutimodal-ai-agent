61e632a772d16bf9ff782e41bf5bca363776c19f
Fatir Yousaf <fatiryousaf@gmail.com>
2025-04-30 12:53:58 -0400

    Made a function tool so the agent can read code files now too

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ from llama_parse import LlamaParse
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, PromptTemplate
 from llama_index.core.embeddings import resolve_embed_model
 from llama_index.core.tools import QueryEngineTool, ToolMetadata
+
+from code_reader import code_reader
 from prompts import context
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv()   # load the .env file for api key
 
 llm = Ollama(model = 'mistral', request_timeout = 60)
 
@@ -17,18 +19,20 @@ parser = LlamaParse(api_key = os.getenv("LLAMACLOUD_API_KEY"), result_type="mark
 
 file_extractor = {".pdf": parser}
 documents = SimpleDirectoryReader("./data", file_extractor=file_extractor).load_data()
+
 embed_model = resolve_embed_model("local:BAAI/bge-m3")
 vector_index = VectorStoreIndex.from_documents(documents, embed_model= embed_model)
 query_engine = vector_index.as_query_engine(llm=llm)
 
 tools = [
     QueryEngineTool(
-        query_engine=query_engine,
-        metadata=ToolMetadata(
-            name="api_documentation",
-            description="this gives documentation about code for an API. Use this for reading docs for the API",
+        query_engine = query_engine,
+        metadata = ToolMetadata(
+            name = "api_documentation",
+            description = "this gives documentation about code for an API. Use this for reading docs for the API",
         ),
-    )
+    ),
+    code_reader
 ]
 
 code_llm = Ollama(model="codellama")
