82cfcd6bcf2a3a39feadef9b07a7cae6033e6a7c
Fatir Yousaf <fatiryousaf@gmail.com>
2025-05-03 18:30:45 -0400

    Added error handling so the agent retries every time any error occurs

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import ast
 import os
 
 from llama_index.core.agent import ReActAgent
@@ -16,7 +17,7 @@ from prompts import context, code_parser_template
 
 load_dotenv()   # load the .env file for api key
 
-llm = Ollama(model = 'mistral', request_timeout = 120)
+llm = Ollama(model = 'mistral', request_timeout = 300)
 
 parser = LlamaParse(api_key = os.getenv("LLAMACLOUD_API_KEY"), result_type="markdown")
 
@@ -46,7 +47,31 @@ json_prompt_template = PromptTemplate(json_prompt_str)
 output_pipeline = QueryPipeline(chain = [json_prompt_template, llm])
 
 while (prompt := input("Enter a prompt (q to quit): ")) != "q":
-    result = agent.query(prompt)
-    next_result = output_pipeline.run(response=result)
-    print(next_result)
+    retries = 0
 
+    while retries < 3:
+        try:
+            result = agent.query(prompt)
+            next_result = output_pipeline.run(response=result)
+            cleaned_json = ast.literal_eval(str(next_result).replace("assistant:", ""))
+        except Exception as e:
+            retries += 1
+            print(f'Retry #{retries} | An error occurred: {e}')
+
+    if retries >= 3:
+        print("Unable to process your input. Try again.")
+        continue
+
+    print(str.upper("Code Generated:"))
+    print(cleaned_json['code'])
+    print(str.upper("Description:"))
+    print(cleaned_json['description'])
+    print(str.upper("Filename:"))
+    print(cleaned_json['filename'])
+
+    try:
+        with open(os.path.join("output", cleaned_json['filename']), "w") as file:
+            file.write(cleaned_json['code'])
+        print(f'Code written to: {cleaned_json["filename"]}')
+    except Exception as e:
+        print(f'Error writing to file: {e}')
